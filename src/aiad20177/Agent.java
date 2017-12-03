480feb14b1dc82401b5b7784c187795d34a01d1d
Rneves3 <ricardo5a23@gmail.com>
2017-12-03 19:25:58 +0000

    Creating Agent class

---
+++ src/aiad20177/Agent.java
@@ -0,0 +1,30 @@
+package aiad20177;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+import java.util.Vector;
+
+import utils.Node;
+
+public class Agent {
+	
+	Node NextStop;
+	protected ArrayList<String> path;
+	//falta variaveis
+	
+	public Agent(int x, int y, int city, ArrayList<Node> Graph) {
+		
+		
+		//inicializar variaveis
+		
+		
+	}
+
+	
+	//metodos get, etc...
+}
